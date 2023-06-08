1e71594892335f8748566b6be75b0542ab2c9372
Yashin Nikolay <kol.yashin@yandex.ru>
2023-06-08 12:25:58 +0300

    parse bug fixed

--- create_bot.py
+++ create_bot.py
@@ -2,5 +2,5 @@ from constants import BOT_TOKEN
 from aiogram import Bot
 from aiogram import Dispatcher
 
-bot: Bot = Bot(token = BOT_TOKEN, parse_mode='HTML')
+bot: Bot = Bot(token = BOT_TOKEN)
 dp: Dispatcher = Dispatcher()
